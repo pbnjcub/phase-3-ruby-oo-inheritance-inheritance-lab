8090f56323e198aeede9c07cb69855a5956a85b4
Paul Jung <pbnjcub@gmail.com>
2022-11-21 14:41:18 -0500

    lab done;
    exit

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,22 @@
-class Student 
+require 'pry'
+
+class Student < User
+    attr_accessor :knowledge
+
+    def initialize(knowledge = [])
+        @knowledge = knowledge
+    end
+
+    def knowledge
+        @knowledge
+    end
+
+    def learn(str)
+        @knowledge << str
+    end
 
 end
+
+# sophie = Student.new
+
+# binding.pry
